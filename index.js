87567527d1517c2d2c5a7d87a5b7476ec134158f
Wm. Todd Wardzinski <wardzinski.todd@gmail.com>
2015-09-16 11:34:34 -0500

    Cleaning up variable names.

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 var accessStats = require("./lib/objectAccess.js");
 
 /**
-* @description Method to dynamically change the time window
-*/
-exports.changeTimeWindow = function(params, cb){
-	accessStats.changeWindow(params, function(err){
-		if (err) {
-			console.log('Change Window Error: ' + err);
-		}
-	});
+ * @description Method to dynamically change the time window
+ */
+exports.changeTimeWindow = function(interval, cb) {
+	if (typeof interval !== 'undefined') {
+		accessStats.changeWindow(interval, function(err) {
+			if (err) {
+				console.log('Change Window Error: ' + err);
+			} else {
+				return cb(null, 'ok');
+			}
+		});
+	} else {
+		return cb('Please provide integer interval.', null);
+	}
 }
 
 /**
-* @description Process to increment access.
-*/
-exports.incAccess = function(params, cb){
-	accessStats.inc(params, function(err){
-		if (err) {
-			console.log('Inc Error: ' + err);
-		}
-	});
+ * @description Process to increment access.
+ */
+exports.incAccess = function(entityName, cb) {
+	if (typeof entityName !== 'undefined') {
+		accessStats.inc(entityName, function(err) {
+			if (err) {
+				console.log('Inc Error: ' + err);
+			}
+		});
+	} else {
+		return cb('Please provide entity name to increment.', null);
+	}
 }
 
 /**
-* @description Process to decrememnt access.
-*/
-exports.decAccess = function(params, cb){
-	accessStats.dec(params, function(err){
-		if (err) {
-			console.log('Dec Error: ' + err);
-		}
-	});
+ * @description Process to decrememnt access.
+ */
+exports.decAccess = function(entityName, cb) {
+	if (typeof entityName !== 'undefined') {
+		accessStats.dec(entityName, function(err) {
+			if (err) {
+				console.log('Dec Error: ' + err);
+			}
+		});
+	} else {
+		return cb('Please provide entity name to decrement.', null);
+	}
 }
